a330b8ebb8ad6dcccd090cb8daf159cccf78e6db
Andrew Maddocks <andrewmadd@icloud.com>
2019-11-12 15:43:44 -0700

    initial commit

---
+++ apple-nav/src/data.js
@@ -0,0 +1,13 @@
+const navArray = ["Mac", "iPad", "iPhone", "watch", "TV", "Music", "Support"];
+const macSub = [
+  "MacBook Air",
+  "MacBook Pro",
+  "iMac",
+  "iMac Pro",
+  "Mac Pro",
+  "Mac mini",
+  "CompareMac models",
+  "Pro Display XDR",
+  "Accessoriesfor Mac",
+  "Catalina"
+];
